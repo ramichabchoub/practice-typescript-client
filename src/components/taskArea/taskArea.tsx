018fe865cc9bedb77f29b435ed5850cdf89ab87f
rami chabchoub <ramichabchoub.rc@gmail.com>
2023-06-12 12:38:08 +0100

    task area

--- src/components/taskArea/taskArea.tsx
+++ src/components/taskArea/taskArea.tsx
@@ -1,10 +1,48 @@
 import { FC, ReactElement } from 'react';
-import { Grid } from '@mui/material';
+import { Grid, Box } from '@mui/material';
+import format from 'date-fns/format';
+import { TaskCounter } from '../taskCounter/taskCounter';
 
 export const TaskArea: FC = (): ReactElement => {
   return (
     <Grid item md={8} px={4}>
-      <h2>Content Area</h2>
+      <Box mb={8} px={4}>
+        {/* <h2>Status Of Your Tasks As On: {new Date().toDateString()}</h2> */}
+        <h2>
+          Status Of Your Tasks As On:{' '}
+          {format(new Date(), 'PPPPP')}
+        </h2>
+      </Box>
+      <Grid
+        container
+        display={'flex'}
+        justifyContent={'center'}
+      >
+        <Grid
+          item
+          display={'flex'}
+          flexDirection={'row'}
+          justifyContent={'space-around'}
+          alignItems={'center'}
+          md={10}
+          xs={12}
+          mb={8}
+        >
+          <TaskCounter />
+          <TaskCounter />
+          <TaskCounter />
+        </Grid>
+        <Grid
+          item
+          display={'flex'}
+          flexDirection={'column'}
+          xs={10}
+          md={8}
+        >
+          <Box>Task Will Come Over Here</Box>
+          <Box>Task Will Come Over Here</Box>
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
